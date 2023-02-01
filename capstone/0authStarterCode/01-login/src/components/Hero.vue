9bd5bfec1abf664715c0e703f0183558676ee30c
Jayson <favrengreen@gmail.com>
2023-02-01 10:11:52 -0500

    Added 0auth starter code

---
+++ capstone/0authStarterCode/01-login/src/components/Hero.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="text-center hero">
+    <img  src="/demologo.png" alt="Vue.js logo" width="220" />
+    <h1 class="mb-4">Project Time</h1>
+    <p class="lead">
+      Start organizing your projects now
+    </p>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "HeroContent"
+};
+</script>
