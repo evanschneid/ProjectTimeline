9115b6893f9dabf722e3f3e87c54c8ab540252c6
Joe <joejr.hasson@gmail.com>
2023-02-04 14:43:17 -0500

    update to single proj page view & trying to connect to dbase

--- capstone/0authStarterCode/01-login/src/views/SingleProjectPage.vue
+++ capstone/0authStarterCode/01-login/src/views/SingleProjectPage.vue
@@ -1,11 +1,38 @@
 <template>
   <div>
       <h1>TESTING SINGLE PROJECT</h1>
+
+      <project-tile />
+      
+      <form>
+        {{projCard}}<br>
+        Proj No.: <input type="text" v-model="projCard.projectID" /><br>
+        Proj Title.: <input type="text" v-model="projCard.projectTitle" /><br>
+        Due Date: <input type="text" v-model="projCard.projectDueDate" />
+      </form>
   </div>
 </template>
 
 <script>
+import ProjectTile from '../components/Dashboard/ProjectTile.vue';
+
 export default {
+  components: { ProjectTile },
+
+  data() {
+    return {
+      currentProject: {}
+    }
+  },
+  created () {
+  this.projectID = this.$route.params.id;
+
+  service.getProject(id).then(
+    (response) => {
+      this.currentProject = response.data;
+        }
+  )
+}
 
 }
 </script>
