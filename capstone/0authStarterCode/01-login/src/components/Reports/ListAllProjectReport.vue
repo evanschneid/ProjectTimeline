1d9340cd8d94324b81cd1bbc514ba18985c9a0e1
Evan Schneider <evanschneid@gmail.com>
2023-02-07 20:10:51 -0500

    pushing report updates

--- capstone/0authStarterCode/01-login/src/components/Reports/ListAllProjectReport.vue
+++ capstone/0authStarterCode/01-login/src/components/Reports/ListAllProjectReport.vue
@@ -1,18 +1,25 @@
 <template>
+<div>
   <v-card>
     <v-card-title>
         Time Reports
-        <v-spacer></v-spacer>
+    <v-spacer></v-spacer>
+        <!-- {{ date }}<br><br>
+        <h3>Debug</h3>
+        {{worklogs}}<br>
+        <h3>Debug</h3>
+        {{filteredWorkLogs2}} -->
+    <Datepicker class="calender" v-model="date" range :partial-range="false" />
 
     <v-text-field
+        class="searchBar"
         v-model="search"
-        append-icon="fa-house"
         label="Search Projects..."
         single-line
         hide-details
     ></v-text-field>
-</v-card-title>
-<v-table>
+  </v-card-title>
+  <v-table>
     <template v-slot:default>
     <thead>
         <tr>
@@ -29,27 +36,54 @@
     </thead>
     <tbody>
         <tr
-        v-for="(item, i) in filteredWorklogs"
+        v-for="(item, i) in filteredWorkLogs"
         :key="i"
         >
         <td>{{ item.id }}</td>
         <td>{{ item.projectTitle }}</td>
-        <td>{{ item.totalTime/60 }} hours</td>
+        <td>{{ item.totalTime }} hours</td>
         </tr>
         
     </tbody>
     </template>
-</v-table>
+  </v-table>
   </v-card>
+</div>
 </template>
+
 <script>
+import { ref } from 'vue';
+import Datepicker from '@vuepic/vue-datepicker';
 import ServerService from '../../services/ServerService'
+
 export default {
+
+  // setup() {
+  //       const date = ref(new Date());
+  //       const format = (date)=> {return date.getDate()}
+
+  //       return {date, format}
+  // },
   name: "worklogReports",
+  components: {
+    Datepicker
+  },
   data() {
+    const date = ref();
+
     return {
+      date,
       worklogs:[], 
-      search: ""
+      worklogFilter: {
+        userId: '',
+        clockIn: '',
+        clockOut: '',
+        projectID: '',
+        totalTime: '',
+        addedComment: '',
+        projectTitle: ''
+      },
+      search: "",
     }
   },
   created () {
@@ -60,35 +94,77 @@ export default {
       } else {
         console.log("No Reports");
       }
-<<<<<<< HEAD
-    )
-    // ServerService.getAllReports().then(response => {
-    //   if (response.data !== undefined) {
-    //     this.worklogs = response.data; 
-    //       console.log(response.data)
-    //     } else {
-    //       console.log("No Reports");
-    //     }
-    //     })
-    //     .catch(error => {
-    //       console.error(error);
-    //     });
-=======
     })
     .catch(error => {
       console.error(error);
     });
->>>>>>> main
   },
+  methods: {
+    // dateFormat(dateAndTime){
+    //   let date1 = dateAndTime[0].split('T');
+    //   let date2 = dateAndTime[1].split('T');
+    //   let dateArray1 = date1.split('-');
+    //   let dateArray2 = date2.split('-');
+    //   return date1[0].concat(' ', date2[0]));
+    // },
+  },
+  
   computed: {
-    filteredWorklogs() {
-      return this.worklogs.filter(item => {
-        return item.projectTitle.toLowerCase().indexOf(this.search.toLowerCase()) > -1
-      });
-    }
+
+    filteredWorkLogs() {
+
+        let curatedLogs = this.worklogs;
+
+        if(this.date != undefined) {
+          curatedLogs =  curatedLogs.filter(
+              (item) => {
+                console.log(typeof String(this.date[0]))
+                console.log(typeof item.clockIn)
+                console.log(this.date[0].getFullYear() + "" + (this.date[0].getMonth()+1) + "" + this.date[0].getDate())
+                let dateFromPicker = String(this.date[0].getFullYear());
+                console.log(`Comparison:' + ${dateFromPicker} + " " + ${item.clockIn} + " " + ${dateFromPicker < item.clockIn} `);
+                return dateFromPicker < item.clockIn
+                // return this.date[0].localeCompare(item.clockIn) < 0;
+              });
+        }
+        if(this.worklogs.projectTitle != "") {
+          curatedLogs = curatedLogs.filter(item => {
+            return item.projectTitle.toLowerCase().indexOf(this.search.toLowerCase()) > -1
+          });
+        }     
+        return curatedLogs;
+    },
+    // filteredWorklogs() {
+    //   let filteredList = this.worklogs;
+
+    //   if(this.worklogs.projectTitle != "") {
+    //     filteredList.filter(item => {
+    //       return item.projectTitle.toLowerCase().indexOf(this.search.toLowerCase()) > -1
+    //       });
+    //   }
+    //   // this one might need to be removed since it is only good for managers techincally....
+    //   if(this.worklogs.userId != "") {
+    //     filteredList.filter(item => {
+    //       return item.userId.toLowerCase().indexOf(this.search.toLowerCase()) > -1
+    //       });
+    //   }
+      
+    //   if(this.date != "") {
+    //     filteredList.filter((item) => {
+    //       return item.clockIn <= this.date[0] && item.clockOut >= this.date[1]
+    //       });
+    //   }
+    //   return filteredList;
+    // },
   }
 }
 </script>
 <style>
+  .calender {
+    margin: 1em;
+  }
 
+  .searchBar {
+    border: black;
+  }
 </style>
---
+++ capstone/0authStarterCode/01-login/src/components/Reports/SingleProjectReportDetails.vue
@@ -0,0 +1,84 @@
+<template>
+<v-card>
+    <v-card-title>
+        {{ worklogs.projectTitle }}
+        <v-spacer></v-spacer>
+        
+        <Datepicker class="calender" v-model="date" range :partial-range="false" />
+
+        <v-text-field
+            class="seachBar"
+            v-model="search"
+            label="Search Date..."
+            single-line
+            hide-details
+        ></v-text-field>
+    </v-card-title>
+    <v-table>
+        <template v-slot:default>
+        <thead>
+            <tr>
+            <th class="text-left">
+                Start Time
+            </th>
+            <th class="text-left">
+                End Time
+            </th>
+            <th class="text-left">
+                Total Hours
+            </th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr
+            v-for="(item, i) in filteredWorklogs"
+            :key="i"
+            >
+            <td>{{ item.clockIn }}</td>
+            <td>{{ item.clockOut }}</td>
+            <td>{{ item.totalTime }} hours</td>
+            </tr>
+            
+        </tbody>
+        </template>
+    </v-table>
+</v-card>
+</template>
+
+<script>
+import { ref } from 'vue';
+import Datepicker from '@vuepic/vue-datepicker';
+import ServerService from '../../services/ServerService'
+
+export default {
+    components: {
+        Datepicker
+    },
+    data() {
+        const date = ref();
+
+        return {
+        date,
+        worklogs:[], 
+        search: "",
+        }
+    },
+    // created () {
+    //     ServerService.getAllReports().then(response => {
+    //     if (response.data !== undefined) {
+    //         this.worklogs = response.data; 
+    //         console.log(response.data)
+    //     } else {
+    //         console.log("No Reports");
+    //     }
+    //     })
+    //     .catch(error => {
+    //         console.error(error);
+    //     });
+    // },
+}
+</script>
+
+<style>
+
+</style>
